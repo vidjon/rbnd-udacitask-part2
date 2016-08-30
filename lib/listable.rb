eb7edef2426820ef178870fc1cdda24aa5829015
vidjon <vidjon@gmail.com>
2016-08-30 12:39:19 -0400

    Format date and priority added to listable

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,22 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+  def format_date(type, options={})
+      type = type.downcase
+    if type == "event"
+        dates = options[:start_date].strftime("%D") if options[:start_date]
+        dates << " -- " + options[:end_date].strftime("%D") if options[:end_date]
+        dates = "N/A" if !dates
+        return dates
+    elsif type == "todo"
+        options[:end_date] ? options[:end_date].strftime("%D") : "No due date"
+    end
+  end
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 end
--- lib/todo.rb
+++ lib/todo.rb
@@ -7,19 +7,9 @@ class TodoItem
     @due = options[:due] ? Date.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
-  def format_date
-    @due ? @due.strftime("%D") : "No due date"
-  end
-  def format_priority
-    value = " ⇧" if @priority == "high"
-    value = " ⇨" if @priority == "medium"
-    value = " ⇩" if @priority == "low"
-    value = "" if !@priority
-    return value
-  end
   def details
     format_description(@description) + "due: " +
-    format_date +
-    format_priority
+    format_date("todo", end_date: @end_date) +
+    format_priority(@priority)
   end
 end
