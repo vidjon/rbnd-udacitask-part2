7b2d73d291232f1e0adf004b9524f065c2bfe008
vidjon <vidjon@gmail.com>
2016-08-31 09:11:06 -0400

    Name of variables and methods fixed

--- app.rb
+++ app.rb
@@ -11,37 +11,37 @@ require_relative "lib/event"
 require_relative "lib/link"
 
 list = UdaciList.new(title: "Julia's Stuff")
-list.add("todo", "Buy more cat food", due: "2016-02-03", priority: "low")
-list.add("todo", "Sweep floors", due: "2016-01-30")
-list.add("todo", "Buy groceries", priority: "high")
-list.add("event", "Birthday Party", start_date: "2016-05-08")
-list.add("event", "Vacation", start_date: "2016-05-28", end_date: "2016-05-31")
-list.add("link", "https://github.com", site_name: "GitHub Homepage")
-list.all
-list.delete(3)
-list.all
+list.add_item("todo", "Buy more cat food", due: "2016-02-03", priority: "low")
+list.add_item("todo", "Sweep floors", due: "2016-01-30")
+list.add_item("todo", "Buy groceries", priority: "high")
+list.add_item("event", "Birthday Party", start_date: "2016-05-08")
+list.add_item("event", "Vacation", start_date: "2016-05-28", end_date: "2016-05-31")
+list.add_item("link", "https://github.com", site_name: "GitHub Homepage")
+list.print_all_items
+list.delete_item(3)
+list.print_all_items
 
-# SHOULD CREATE AN UNTITLED LIST AND ADD ITEMS TO IT
+# SHOULD CREATE AN UNTITLED LIST AND add_item ITEMS TO IT
 # --------------------------------------------------
  new_list = UdaciList.new # Should create a list called "Untitled List"
- new_list.add("todo", "Buy more dog food", due: "in 5 weeks", priority: "medium")
- new_list.add("todo", "Go dancing", due: "in 2 hours")
- new_list.add("todo", "Buy groceries", priority: "high")
- new_list.add("event", "Birthday Party", start_date: "May 31")
- new_list.add("event", "Vacation", start_date: "Dec 20", end_date: "Dec 30")
- new_list.add("event", "Life happens")
- new_list.add("link", "https://www.udacity.com/", site_name: "Udacity Homepage")
- new_list.add("link", "http://ruby-doc.org")
+ new_list.add_item("todo", "Buy more dog food", due: "in 5 weeks", priority: "medium")
+ new_list.add_item("todo", "Go dancing", due: "in 2 hours")
+ new_list.add_item("todo", "Buy groceries", priority: "high")
+ new_list.add_item("event", "Birthday Party", start_date: "May 31")
+ new_list.add_item("event", "Vacation", start_date: "Dec 20", end_date: "Dec 30")
+ new_list.add_item("event", "Life happens")
+ new_list.add_item("link", "https://www.udacity.com/", site_name: "Udacity Homepage")
+ new_list.add_item("link", "http://ruby-doc.org")
 
 # SHOULD RETURN ERROR MESSAGES
 # ----------------------------
- #new_list.add("image", "http://ruby-doc.org") # Throws InvalidItemType error
- #new_list.delete(9) # Throws an IndexExceedsListSize error
- #new_list.add("todo", "Hack some portals", priority: "super high") # throws an InvalidPriorityValue error
+ #new_list.add_item("image", "http://ruby-doc.org") # Throws InvalidItemType error
+ #new_list.delete_item(9) # Throws an IndexExceedsListSize error
+ #new_list.add_item("todo", "Hack some portals", priority: "super high") # throws an InvalidPriorityValue error
 
 # DISPLAY UNTITLED LIST
 # ---------------------
- new_list.all
+ new_list.print_all_items
 
 
 # DEMO FILTER BY ITEM TYPE
@@ -49,6 +49,6 @@ list.all
 new_list.filter("event")
 
 # GET TASKDUE WITHIN THE NEXT DAYS
-new_list.itemsDueWithinNextOfDays(3)
-new_list.itemsDueWithinNextOfDays(40)
-new_list.printItemsInTable
+new_list.items_due_within_next_no_of_days(3)
+new_list.items_due_within_next_no_of_days(40)
+new_list.print_items_in_table_format
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -9,34 +9,29 @@ class UdaciList
     @supportedTypes = ["todo", "event", "link"]
     @supportedPriority = ["low", "medium", "high"]
   end
-  def add(type, description, options={})
+  def add_item(type, description, options={})
     type = type.downcase
-
     raise UdaciListErrors::InvalidItemType, "Invalid type to add to list" if !(@supportedTypes.include? type)
     raise UdaciListErrors::InvalidPriorityValue, "Invalid priority type" if (options[:priority] && !(@supportedPriority.include? options[:priority]))
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
   end
-  def delete(index)
-     puts @items.size
-    raise UdaciListErrors::IndexExceedsListSize, "asfs" if index.to_i > @items.size
+  def delete_item(index)
+    raise UdaciListErrors::IndexExceedsListSize, "Index exceeds the size of the list" if index.to_i > @items.size
     @items.delete_at(index - 1)
   end
-  def all
+  def print_all_items
     puts "-" * @title.length
     puts @title
     puts "-" * @title.length
-    printItems(items: @items)
-    #@items.each_with_index do |item, position|
-     # puts "#{position + 1}) #{item.details}"
-    #end
+    print_items(items: @items)
   end
   def filter(item_filter)
-      items_filtered = @items.select{ |e| (e.item_type.downcase == item_filter.downcase) }
-      items_filtered.empty? ? (puts "No items with item type: #{item_filter}") : printItems(items: items_filtered, header: "Items with filter: #{item_filter}")
+      items_filtered = @items.select{ |item| (item.item_type.downcase == item_filter.downcase) }
+      items_filtered.empty? ? (puts "No items with item type: #{item_filter}") : print_items(items: items_filtered, header: "Items with filter: #{item_filter}")
   end
-  def printItems(options={})
+  def print_items(options={})
       puts options[:header] if options[:header]
       if options[:items]
           options[:items].each_with_index do |item, position|
@@ -44,28 +39,27 @@ class UdaciList
         end
       end
   end
-  def itemsDueWithinNextOfDays(number_of_days)
+  def items_due_within_next_no_of_days(number_of_days)
       if number_of_days.is_a? Integer
-          puts (Date.today + 10).strftime("%D")
-          todo_items = @items.select { |e| (e.item_type.downcase == "todo") && e.due &&
-              (Chronic.parse(Date.today.strftime("%D")) <= e.due) && (e.due <= Chronic.parse((Date.today + number_of_days).strftime("%D"))) }
-          todo_items.empty? ? (puts "No items todo within the next #{number_of_days} days" ) : printItems(items: todo_items, header: "Items due within the next #{number_of_days} days")
+          todo_items = @items.select { |item| (item.item_type.downcase == "todo") && item.due &&
+              (Chronic.parse(Date.today.strftime("%D")) <= item.due) && (item.due <= Chronic.parse((Date.today + number_of_days).strftime("%D"))) }
+          todo_items.empty? ? (puts "No items todo within the next #{number_of_days} days" ) : print_items(items: todo_items, header: "Items due within the next #{number_of_days} days")
       else
           Puts "Enter valid number of days"
       end
   end
-  def printItemsInTable
+  def print_items_in_table_format
       for item_type in @supportedTypes
-          a = Array.new
+          items_array = Array.new
           puts "#{item_type}"
-          for item in @items.select{|e| e.item_type.downcase == item_type}
-              h = Hash.new
+          for item in @items.select{|item| item.item_type.downcase == item_type}
+              item_hash = Hash.new
               for item_instance in item.instance_variables
-                  h[item_instance] = item.instance_variable_get(item_instance)
+                  item_hash[item_instance] = item.instance_variable_get(item_instance)
               end
-              a << h
+              items_array << item_hash
           end
-          Formatador.display_table(a)
+          Formatador.display_table(items_array)
       end
   end
 end
